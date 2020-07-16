6ab6e1cbc589a76eb6982c700a29fc13ed7d2c09
mnhudaqbd <mnhuda8@gmail.com>
2020-07-16 18:58:06 +0600

    hero section added

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -3,21 +3,7 @@
 
 @section('main')
 
-<!-- ========================= SECTION INTRO ========================= -->
-<section class="section-intro padding-y-sm">
-<div class="container">
-
-<div class="intro-banner-wrap">
-    <img src="images/banners/1.jpg" class="img-fluid rounded">
-</div>
-
-</div> <!-- container //  -->
-</section>
-<!-- ========================= SECTION INTRO END// ========================= -->
-
-
-
-
+    @include('frontend.partials._hero')
 
 <!-- ========================= SECTION CONTENT ========================= -->
 <section class="section-content">
---
+++ resources/views/frontend/partials/_hero.blade.php
@@ -0,0 +1,11 @@
+<!-- ========================= SECTION INTRO ========================= -->
+<section class="section-intro padding-y-sm">
+    <div class="container">
+
+        <div class="intro-banner-wrap">
+            <img src="images/banners/1.jpg" class="img-fluid rounded">
+        </div>
+
+    </div> <!-- container //  -->
+</section>
+<!-- ========================= SECTION INTRO END// ========================= -->
